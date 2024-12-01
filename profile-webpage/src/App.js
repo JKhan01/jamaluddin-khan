4ee629842549b7bac965163f43f8a44b545dc96d
JKhan01 <jkhan266@gmail.com>
2024-11-30 16:26:17 -0800

    Completed landing page with contact info. Added navigation links. Refactored files.

--- profile-webpage/src/App.js
+++ profile-webpage/src/App.js
@@ -5,20 +5,24 @@ import ContactInfo from './components/ContactInfo/ContactInfo';
 function App() {
   return (
     <div className="App">
-      
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Mohammed J Khan
-        </a> */}
-      <ContactInfo></ContactInfo>
+      <div className="row empty-container">
+        <div className="col-sm-2 empty-container">
+          <h2>Logo</h2>
+          <h4>Headline</h4>
+        </div>
+        <div className='col-sm-8 main-container'>
+          <ContactInfo></ContactInfo>
+        </div>
+        <div className="col-sm-2 empty-container list-group">
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">About Me</a></div>
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">Experience</a></div>
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">Projects</a></div>
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">Recommendations</a></div>
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">Blogs</a></div>
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">Let's Talk!</a></div>
+            <div className="nav-links-container"><a href="#About" className="list-group-item list-group-item-action list-group-item-info">Freelance</a></div>
+        </div>
+      </div>
     </div>
   );
 }
